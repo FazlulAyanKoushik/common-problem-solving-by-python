41aaee4a94dc3350f248a9d9f37b428b79d8d376
fazlulAyanKoushik <ak.fazlulayan177@gmail.com>
2024-07-16 19:59:57 +0600

    most freq

--- src/arrays_strings/most_frequent_even_number.py
+++ src/arrays_strings/most_frequent_even_number.py
@@ -25,4 +25,35 @@ from typing import List
 
 class Solution:
     def mostFrequentEven(self, nums: List[int]) -> int:
-        pass
+        counts = {}
+        for num in nums:
+            if num % 2 == 0:
+                counts[num] = counts.get(num, 0) + 1
+
+        if not counts:
+            return -1
+
+        highest_freq = 0
+        smallest_num = 0
+        for num, freq in counts.items():
+            if freq > highest_freq:
+                highest_freq = freq
+                smallest_num = num
+            elif freq == highest_freq and num < smallest_num:
+                smallest_num = num
+        return smallest_num
+
+
+if __name__ == '__main__':
+    # Example usage:
+    array = [1, 2, 2, 3, 3, 3]
+    solution = Solution()
+    print(solution.mostFrequentEven(array))
+    array = [1, 1, 1, 3, 3, 3]
+    print(solution.mostFrequentEven(array))
+    array = [1, 2, 2, 3, 3, 3, 4, 4, 4]
+    print(solution.mostFrequentEven(array))
+    array = [1, 2, 2, 2, 2, 3, 3, 4, 4, 4, 4]
+    print(solution.mostFrequentEven(array))
+    array = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    print(solution.mostFrequentEven(array))
